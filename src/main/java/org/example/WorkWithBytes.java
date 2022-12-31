57520a280f55b073adfe4c346b851b50fc2d0e34
Марк Міллер <youtte1g@gmail.com>
2022-12-31 09:54:22 +0200

    write to file using FileOutPutStream

--- src/main/java/org/example/WorkWithBytes.java
+++ src/main/java/org/example/WorkWithBytes.java
@@ -1,27 +1,17 @@
 package org.example;
 
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-        FileWriter writer = null;
-        try {
-            writer = new FileWriter(fileName, true);
-            for (byte numbers : data) {
-                writer.write(numbers);
-            }
+        try (FileOutputStream stream = new FileOutputStream(fileName)) {
+            stream.write(data);
         } catch (IOException e) {
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (writer != null) {
-                    writer.close();
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
         }
     }
 }
